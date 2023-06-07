fa646b078f0f38c446ef124f808cd4c2f57bb87e
Adam DEZIRI <adam.deziri@epitech.eu>
2023-06-07 15:58:36 +0200

    fix(vault): avoid critical error during unsealing when restarting vault instance

--- vault/unseal-script.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-VAULT_CACERT='/vault/certs/cert.pem' VAULT_ADDR='https://secure-env.poc-innovation.com:8200/' vault operator init -format "json" -key-shares 1 -key-threshold 1 > /vault/config/.init_data
-VAULT_CACERT='/vault/certs/cert.pem' VAULT_ADDR='https://secure-env.poc-innovation.com:8200/' vault operator unseal $(cat /vault/config/.init_data | jq -r '.unseal_keys_b64[0]')
