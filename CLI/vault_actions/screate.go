2db00b228c5a5626bc503f1d5afe9ccd370a9d93
Gatien RITTNER <gatien.rittner@epitech.eu>
2023-06-11 14:55:25 +0200

    fix(screate) : create a new secret without deleting others

--- CLI/vault_actions/screate.go
+++ CLI/vault_actions/screate.go
@@ -8,10 +8,29 @@ import (
 )
 
 func Screate(name string, secretData map[string]interface{}, client *vault.Client) bool {
-
-	_, err := client.KVv2("secret").Put(context.Background(), name, secretData)
+	existingSecret, err := client.KVv2("secret").Get(context.Background(), name)
 	if err != nil {
-		log.Fatalf("unable to write secret: %v", err)
+		log.Fatalf("unable to read secret: %v", err)
+	}
+
+	if existingSecret != nil {
+		existingData := existingSecret.Data
+		for k, v := range secretData {
+			existingData[k] = v
+		}
+
+		_, err = client.KVv2("secret").Put(context.Background(), name, existingData)
+		if err != nil {
+			log.Fatalf("unable to write secret: %v", err)
+		}
+		fmt.Println("deja un secret existant")
+
+	} else {
+		_, err = client.KVv2("secret").Put(context.Background(), name, secretData)
+		if err != nil {
+			log.Fatalf("unable to write secret: %v", err)
+		}
+		fmt.Println("pas de secret existant")
 	}
 
 	fmt.Println("Secret written successfully.")
