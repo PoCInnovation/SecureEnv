ff798705102b20e5bec0abaf53cc23f749d567a0
Gatien RITTNER <gatien.rittner@epitech.eu>
2023-06-15 21:57:45 +0200

    fix(main.go) : remove vault api, using antonin's api

--- CLI/main.go
+++ CLI/main.go
@@ -1,15 +1,12 @@
 package main
 
 import (
-	"cli/parse_file"
 	"cli/vault_actions"
 	"context"
 	"flag"
 	"fmt"
-	"log"
 	"os"
 
-	vault "github.com/hashicorp/vault/api"
 	"github.com/peterbourgon/ff/v3/ffcli"
 )
 
@@ -17,22 +14,12 @@ func main() {
 
 	rootFlagSet := flag.NewFlagSet("textctl", flag.ExitOnError)
 
-	secure_env := parse_file.Parsefile()
-
-	config := vault.DefaultConfig()
-	config.Address = secure_env.Host
-	client, err := vault.NewClient(config)
-
-	if err != nil {
-		log.Fatalf("unable to initialize Vault client: %v", err)
-	}
-	client.SetToken(secure_env.Token)
+	mainUrl := "http://0.0.0.0:8080/project"
 
 	root := &ffcli.Command{
-		ShortUsage: "textctl [flags] <subcommand>",
-		FlagSet:    rootFlagSet,
-		Subcommands: []*ffcli.Command{vault_actions.Sdelete_var(client), vault_actions.Screate_var(client),
-			vault_actions.Sget_var(client), vault_actions.Spush_var(client)},
+		ShortUsage:  "textctl [flags] <subcommand>",
+		FlagSet:     rootFlagSet,
+		Subcommands: []*ffcli.Command{vault_actions.Sget_var(mainUrl), vault_actions.Sdelete_var(mainUrl)},
 		Exec: func(context.Context, []string) error {
 			return flag.ErrHelp
 		},
