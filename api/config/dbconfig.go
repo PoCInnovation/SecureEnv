593159ad5a303fddd6a743403f449402ff7902a8
Matribuk <antonin.leprest@epitech.eu>
2023-08-16 16:31:27 +0200

    fix(db): unnecessary files and rm .env, add load config from env
    Signed-off-by: Matribuk <antonin.leprest@epitech.eu>

--- api/config/dbconfig.go
+++ api/config/dbconfig.go
@@ -2,11 +2,37 @@ package config
 
 import (
 	"database/sql"
+
+	"os"
+
 	_ "github.com/go-sql-driver/mysql"
+
+	"log"
+
+	"github.com/joho/godotenv"
 )
 
+var db_name string = ""
+var db_port string = ""
+var db_user string = ""
+var db_pass string = ""
+
+func getEnvDB() {
+
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+
+	db_name = os.Getenv("SECURE_ENV_DB_NAME")
+	db_port = os.Getenv("SECURE_ENV_DB_PORT")
+	db_user = os.Getenv("SECURE_ENV_DB_USER")
+	db_pass = os.Getenv("SECURE_ENV_DB_PASSWORD")
+}
+
 func ConnectDB() (*sql.DB, error) {
-	db, err := sql.Open("mysql", "root:38630@tcp(localhost:3306)/secureenv")
+	getEnvDB()
+	db, err := sql.Open("mysql", db_user+":"+db_pass+"@tcp(localhost:"+db_port+")/"+db_name)
 	if err != nil {
 		return nil, err
 	}
--- api/go.mod
+++ api/go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/gin-gonic/gin v1.9.1
 	github.com/go-sql-driver/mysql v1.7.1
 	github.com/hashicorp/vault/api v1.9.2
+	github.com/joho/godotenv v1.5.1
 )
 
 require (
