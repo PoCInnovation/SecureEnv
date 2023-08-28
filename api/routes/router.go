26d80c66332998a7274d3dbf70e67d3e24858911
Gatien RITTNER <gatien.rittner@epitech.eu>
2023-08-28 20:20:36 +0200

    feat(db): pass db into routes
    Signed-off-by: Gatien RITTNER <gatien.rittner@epitech.eu>

--- api/routes/router.go
+++ api/routes/router.go
@@ -9,13 +9,25 @@ func ApplyRoutes(router *gin.Engine, db *sql.DB) {
 	router.GET("/project/", func(c *gin.Context) {
 		project_list(c, db)
 	})
-	router.POST("/project/", project_create)
-	router.PUT("/project/:project/", project_edit)
-	router.DELETE("/project/:project/", project_del)
+	router.POST("/project/", func(c *gin.Context) {
+		project_create(c, db)
+	})
+	router.PUT("/project/:project/", func(c *gin.Context) {
+		project_edit(c, db)
+	})
+	router.DELETE("/project/:project/", func(c *gin.Context) {
+		project_del(c, db)
+	})
 	router.GET("/project/:project/", project_get)
 	router.PATCH("/project/:project/", project_update)
 	router.GET("/project/:project/var/", var_list)
-	router.POST("/project/:project/var/:variable", var_add)
-	router.PATCH("/project/:project/var/:variable", var_edit)
-	router.DELETE("/project/:project/var/:variable", var_del)
+	router.POST("/project/:project/var/:variable", func(c *gin.Context) {
+		var_add(c, db)
+	})
+	router.PATCH("/project/:project/var/:variable", func(c *gin.Context) {
+		var_edit(c, db)
+	})
+	router.DELETE("/project/:project/var/:variable", func(c *gin.Context) {
+		var_del(c, db)
+	})
 }
