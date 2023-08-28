989852d52150f92bba05fb9b66496c74f44dda96
Gatien RITTNER <gatien.rittner@epitech.eu>
2023-08-28 20:19:53 +0200

    feat(db): put project id in delete cascade
    Signed-off-by: Gatien RITTNER <gatien.rittner@epitech.eu>

--- schema.sql
+++ schema.sql
@@ -8,5 +8,5 @@ CREATE TABLE logs (
     ip_adress VARCHAR(255) NOT NULL,
     action VARCHAR(255) NOT NULL,
     project_id INT NOT NULL,
-    FOREIGN KEY (project_id) REFERENCES projects(id)
+    FOREIGN KEY (project_id) REFERENCES projects(id) ON DELETE CASCADE
 );
