55f051aa18aa48a02ed5aa1b9ba79aa96bba4176
Gatien RITTNER <gatien.rittner@epitech.eu>
2023-08-28 18:33:46 +0200

    feat(db): Change sql schema
    Signed-off-by: Gatien RITTNER <gatien.rittner@epitech.eu>

--- schema.sql
+++ schema.sql
@@ -3,24 +3,10 @@ CREATE TABLE projects (
     name VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE users (
+CREATE TABLE logs (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL
-);
-
-CREATE TABLE user_projects (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    user_id INT NOT NULL,
+    ip_adress VARCHAR(255) NOT NULL,
+    action VARCHAR(255) NOT NULL,
     project_id INT NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES users (id),
-    FOREIGN KEY (project_id) REFERENCES projects (id)
-);
-
-CREATE TABLE actions (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    user_id INT NOT NULL,
-    project_id INT NOT NULL,
-    action_description VARCHAR(255) NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES users (id),
-    FOREIGN KEY (project_id) REFERENCES projects (id)
+    FOREIGN KEY (project_id) REFERENCES projects(id)
 );
